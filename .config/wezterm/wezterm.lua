ba2f7b08aa2fa982f4dca91c72966ba44ae0ad90
ShotaArima <g2172010@tcu.ac.jp>
2026-07-18 01:14:18 +0900

    fix wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,13 +7,18 @@ config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18.0
 
 -- 背景を透過
-config.window_background_opacity = 0.65
+config.window_background_opacity = 0.5
 -- ぼかしを追加
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 1
 
 -- other settings
-require("keymaps").apply_to_config(config)
 require("appearance").apply_to_config(config)
 require("tab").apply_to_config(config)
+
+-- keybinds.lua は { keys = ..., key_tables = ... } の素のテーブルを返すため、
+-- apply_to_config ではなく直接マージする
+local keybinds = require("keybinds")
+config.keys = keybinds.keys
+config.key_tables = keybinds.key_tables
 
 return config
