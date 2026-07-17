b2dbf4b230eac118da67de984380c9c4e9a8278c
ShotaArima <g2172010@tcu.ac.jp>
2026-07-17 19:40:56 +0900

    other settings files

---
+++ .config/wezterm/keymaps.lua
@@ -0,0 +1,5 @@
+local module = {}
+function module.apply_to_config(config)
+  config.keys = { --[[ ここに集約 ]] }
+end
+return module
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,4 +16,9 @@ config.window_background_opacity = 0.85
 -- ぼかしを追加
 config.macos_window_background_blur = 20
 
+-- other settings
+require("keymaps").apply_to_config(config)
+require("appearance").apply_to_config(config)
+require("tab").apply_to_config(config)
+
 return config
