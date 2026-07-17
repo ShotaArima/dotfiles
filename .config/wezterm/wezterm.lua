4e4dd923712f55300e281d503d250ad32c1ec920
ShotaArima <g2172010@tcu.ac.jp>
2026-07-17 19:36:19 +0900

    add font settings

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+
+-- config.font_size = 14.0
+-- config.font = wezterm.font( 
+--   "HackGen Console NF" 
+-- ) 
+
+config.font = wezterm.font("JetBrains Mono") 
+config.font_size = 14.0
+
+-- 背景を透過
+config.window_background_opacity = 0.85
+-- ぼかしを追加
+config.macos_window_background_blur = 20
+
+return config
